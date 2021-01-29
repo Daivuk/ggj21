7d4caea47434287240e15e62fa42273b6db619d5
Chris Sullivan <chris.tomas.sullivan@gmail.com>
2021-01-29 16:20:25 -0500

    cat animation added room fixes, box controller and interaction started

---
+++ Lost&Found2021/Assets/Environment/Script/ActivatonZone.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivatonZone : MonoBehaviour
+{
+    [HideInInspector] public CapsuleCollider2D circleCollider;
+
+    public void Awake()
+    {
+        circleCollider = GetComponent<CapsuleCollider2D>();
+    }
+
+    //can use this for highlighting
+}
---
+++ Lost&Found2021/Assets/Environment/Script/BoxControllerObject.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxControllerObject : Interactable
+{
+    public GameObject brokenPeicePrefab;
+    public Sprite[] shards;
+    public int maxBreakAblePiece = 5;
+    //public ItemDropManager itemManager;
+    private float ItemSpawnTimer;
+
+    public void Awake()
+    {
+        ItemSpawnTimer = 5f;
+        //interactionDistance = 5;
+    }
+    public override void Interact()
+    {
+        //base.Interact();
+    }
+
+    public void Damage()
+    {
+        //create a shatter 5 pease
+        int randomPiece = Random.Range(2, maxBreakAblePiece);
+
+        for (int i = 0; i < randomPiece; i++)
+        {
+            int random = Random.Range(0, shards.Length);
+            float OffsetX = Random.Range(-0.5f, 0.5f);
+            float OffsetY = Random.Range(-0.5f, 0.5f);
+            Instantiate(brokenPeicePrefab, new Vector3(transform.position.x + OffsetX, transform.position.y + OffsetY), transform.rotation).GetComponent<BrokenPieces>().setUp(shards[random], 30f);
+        }
+
+        //spawn item;
+        //Item item = itemManager.getDrop();
+        /*
+        if (item != null)
+        {
+            //ItemPickUpHandler.createTimedPickUp(GameController.instance.ItemPickUpPrefab, item, gameObject.transform, ItemSpawnTimer);
+        }
+        */
+
+        Destroy(this.gameObject);
+    }
+}
