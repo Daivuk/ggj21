b7efe2b9f269de2f5a24fecb5cfb211bcdc5e230
Daivuk <daivuk@gmail.com>
2021-01-30 13:25:30 -0500

    Started decorating rooms with stuff, pausing

---
+++ LostAndFound2021/Assets/RandomChance.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomChance : MonoBehaviour
+{
+    public float Chances = 0.5f; // 50%
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // I know this is kind of hacky, but it allows me to quickly pick a chance of ANYTYHING in a room.
+        // I wouldnt publish a product with this, but.. gamejam!
+        if (Random.Range(0.0f, 1.0f) > Chances)
+        {
+            Destroy(gameObject);
+        }
+        Destroy(this);
+    }
+}
